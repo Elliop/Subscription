96312c8af5404f8c5a0c5bea7fbfa91145ec4156
Elliop <abdelmouhayminegx@gmail.com>
2021-11-25 22:47:07 +0100

    Init Backend

---
+++ Server/src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(8080, () => {
+  console.log(`Now listening to port 8080`);
+});
