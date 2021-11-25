81bf0e1bdee6c7f32d295d4f6fa4ae366e61edf0
Elliop <abdelmouhayminegx@gmail.com>
2021-11-25 16:52:52 +0100

    First Commit

---
+++ Client/src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Nav/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/*  */}
+    </div>
+  );
+}
+
+export default App;
---
+++ Client/src/components/Nav/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="">
+      <nav className="">home</nav>
+    </div>
+  );
+};
+
+export default Navbar;
