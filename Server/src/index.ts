79c2c7e678ea21965cd6ef2f8c4a259945abe826
Elliop <abdelmouhayminegx@gmail.com>
2021-11-26 14:08:31 +0100

    Co to mongodb

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import authRoutes from "./routes/auth";
-const app = express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-app.use(express.json());
-app.use("/auth", authRoutes);
+mongoose
+  .connect(process.env.MONGO_URL as string)
+  .then(() => {
+    console.log("DB Connected");
+    const app = express();
+    app.use(express.json());
+    app.use("/auth", authRoutes);
 
-app.listen(8080, () => {
-  console.log(`Now listening to port 8080`);
-});
+    app.listen(8080, () => {
+      console.log(`Now listening to port 8080`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
