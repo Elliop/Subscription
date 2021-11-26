ad6fcfe2d97a65b39d9451c9f51b1ac0ef5fecae
Elliop <abdelmouhayminegx@gmail.com>
2021-11-26 17:17:45 +0100

    Login route

--- Server/src/routes/auth.ts
+++ Server/src/routes/auth.ts
@@ -40,8 +40,74 @@ router.post(
       email,
       password: hashedPassword,
     });
-    res.json(user);
+    const token = await JWT.sign(
+      { email: newUser.email },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: 360000,
+      }
+    );
+
+    res.json({
+      errors: [],
+      data: {
+        token,
+        user: {
+          id: newUser._id,
+          email: newUser.email,
+        },
+      },
+    });
   }
 );
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.json({
+      errors: [
+        {
+          msg: "Invalids credentials",
+        },
+      ],
+      data: null,
+    });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.json({
+      errors: [
+        {
+          msg: "Invalids credentials",
+        },
+      ],
+      data: null,
+    });
+  }
+
+  const token = await JWT.sign(
+    { email: user.email },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: 360000,
+    }
+  );
+
+  return res.json({
+    errors: [],
+    data: {
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+      },
+    },
+  });
+});
 
 export default router;
