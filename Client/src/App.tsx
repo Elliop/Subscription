18fede424fd232c17f4c5e62f84e230cfacca20e
Elliop <abdelmouhayminegx@gmail.com>
2021-11-27 17:15:05 +0100

    Add Protected Route

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Nav/Navbar";
 import Articles from "./pages/Articles";
 import ArticlesPlan from "./pages/ArticlesPlan";
 import LandingPage from "./pages/LandingPage";
+import { ProtectedRoute } from "./routes/ProtectedRoute";
 
 function App() {
   return (
@@ -10,10 +11,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/articles">
+        <Route path="/articles" element={<ProtectedRoute />}>
           <Route path="/articles" element={<Articles />} />
         </Route>
-        <Route path="/article-plans">
+        <Route path="/article-plans" element={<ProtectedRoute />}>
           <Route path="/article-plans" element={<ArticlesPlan />} />
         </Route>
       </Routes>
