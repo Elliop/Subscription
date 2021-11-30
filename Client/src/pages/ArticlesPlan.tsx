41d6d0b5b6f8eab20da928132033bdd8b0250093
Elliop <abdelmouhayminegx@gmail.com>
2021-11-30 16:34:49 +0100

    Show contents from the server to Articles Plan page

--- Client/src/pages/ArticlesPlan.tsx
+++ Client/src/pages/ArticlesPlan.tsx
@@ -1,8 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const ArticlesPlan = () => {
+  const [prices, setPrices] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchPrices();
+  }, []);
+
+  const fetchPrices = async () => {
+    const { data: response } = await axios.get(
+      "http://localhost:8080/subs/prices"
+    );
+    console.log(response);
+    setPrices(response.data);
+  };
+
   return (
-    <div>
-      {/*  */}
-      {/*  */}
+    <div className="w-full pt-8">
+      <div className="flex flex-col sm:flex-row justify-center mb-6 sm:mb-0">
+        {prices.map((price: any) => (
+          <div className="sm:flex-1 lg:flex-initial lg:w-1/4 rounded-t-lg rounded-md bg-blue-200 mt-4 flex flex-col mx-6">
+            <div className="p-8 text-3xl font-bold text-center">
+              ${price.unit_amount / 100}
+            </div>
+            <div className="p-8 text-3xl font-bold text-center">
+              {price.nickname}
+            </div>
+            <button className="inline-block bg-green-300 text-white px-6 py-4 rounded hover:bg-green-darker hover:text-white hover:no-underline">
+              Buy Now
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
