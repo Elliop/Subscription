ac8a641f9d601879698bef6378b5ff3c52b446d3
Elliop <abdelmouhayminegx@gmail.com>
2021-11-30 15:50:54 +0100

    Add Stripe

---
+++ Server/src/routes/subs.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { checkAuth } from "../middleware/checkAuth";
+import { stripe } from "../utils/stripe";
+
+const router = express.Router();
+
+router.get("/prices", checkAuth, async (req, res) => {
+  const prices = await stripe.prices.list({
+    apiKey: process.env.STRIPE_SECRET_KEY,
+  });
+
+  return res.json(prices);
+});
+
+export default router;
