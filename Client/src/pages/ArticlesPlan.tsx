48c7a81655406a04c2a57f7ca7b349de2360ea1e
Elliop <abdelmouhayminegx@gmail.com>
2021-11-30 18:41:17 +0100

    change color depending on plan

--- Client/src/pages/ArticlesPlan.tsx
+++ Client/src/pages/ArticlesPlan.tsx
@@ -15,19 +15,27 @@ const ArticlesPlan = () => {
     console.log(response);
     setPrices(response.data);
   };
+  const backgroundColors: any = {
+    Basic: "rgb(104, 219, 104)",
+    Standard: "rgb(185, 42, 23, 0.835)",
+    Premium: "pink",
+  };
 
   return (
     <div className="w-full pt-8">
       <div className="flex flex-col sm:flex-row justify-center mb-6 sm:mb-0">
         {prices.map((price: any) => (
-          <div className="sm:flex-1 lg:flex-initial lg:w-1/4 rounded-t-lg rounded-md bg-blue-200 mt-4 flex flex-col mx-6">
+          <div
+            style={{ backgroundColor: backgroundColors[price.nickname] }}
+            className="sm:flex-1 lg:flex-initial lg:w-1/4 rounded-t-lg rounded-md mt-4 flex flex-col mx-6"
+          >
             <div className="p-8 text-3xl font-bold text-center">
               ${price.unit_amount / 100}
             </div>
             <div className="p-8 text-3xl font-bold text-center">
               {price.nickname}
             </div>
-            <button className="inline-block bg-green-300 text-white px-6 py-4 rounded hover:bg-green-darker hover:text-white hover:no-underline">
+            <button className="inline-block bg-blue-300 text-white px-6 py-4 rounded hover:bg-green-darker hover:text-white hover:no-underline">
               Buy Now
             </button>
           </div>
