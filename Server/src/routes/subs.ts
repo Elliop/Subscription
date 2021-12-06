351ef6e3b728d286b3cc82f2a5fd2e4cea69c116
Elliop <abdelmouhayminegx@gmail.com>
2021-12-06 14:06:08 +0100

    Add some articles to the DB

--- Server/src/routes/subs.ts
+++ Server/src/routes/subs.ts
@@ -2,6 +2,7 @@ import express from "express";
 import User from "../models/user";
 import { checkAuth } from "../middleware/checkAuth";
 import { stripe } from "../utils/stripe";
+// import Article from "../models/article";
 
 const router = express.Router();
 
@@ -15,6 +16,17 @@ router.get("/prices", checkAuth, async (req, res) => {
 
 router.post("/session", checkAuth, async (req, res) => {
   const user = await User.findOne({ email: req.user });
+
+  // Create an article
+
+  // Article.create({
+  //   title: "The Secret Behind Success",
+  //   imageUrl:
+  //     "https://images.unsplash.com/photo-1599420186946-7b6fb4e297f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw0MHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
+  //   content:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultricies lacus vitae lacus posuere malesuada. In metus dolor, vulputate ac mollis eget, efficitur quis elit. Fusce dignissim sapien non tincidunt facilisis. Nunc dictum tincidunt consectetur. Duis sed turpis ut quam viverra suscipit nec sit amet lacus. Curabitur molestie metus quis eleifend varius.",
+  //   access: "Premium",
+  // });
 
   const session = await stripe.checkout.sessions.create(
     {
